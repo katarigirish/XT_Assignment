4e5ae63bd14277d893f91ff38ca31b13db3c7626
Girish Katari <katarigirish@gmail.com>
2020-06-26 18:03:31 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'react-charts';
-import moment from 'moment';
+import React, { useState, useEffect } from "react";
+import { Chart } from "react-charts";
+import moment from "moment";
 
 function App() {
   const [loading, setLoading] = useState(() => false);
   const [page, setPage] = useState(() => 0);
   const [state, setState] = useState(() => ({}));
-  const data = React.useMemo(() => [
-    {
-      label: 'ID',
-      data: state?.hits?.map((hit, index) => ([index, hit.objectID ?? 0])) ?? [],
-    },
-    {
-      label: 'Votes',
-      data: state?.hits?.map((hit, index) => ([index, hit.points ?? 0])) ?? [],
-    },
-  ], [state]);
-  const axes = React.useMemo(() => [
-    { primary: true, type: 'linear', position: 'bottom' },
-    { type: 'linear', position: 'left' },
-  ], []);
+  const data = React.useMemo(
+    () => [
+      {
+        label: "ID",
+        data: state?.hits?.map((hit, index) => [index, hit.objectID ?? 0]) ?? []
+      },
+      {
+        label: "Votes",
+        data: state?.hits?.map((hit, index) => [index, hit.points ?? 0]) ?? []
+      }
+    ],
+    [state]
+  );
+  const axes = React.useMemo(
+    () => [
+      { primary: true, type: "linear", position: "bottom" },
+      { type: "linear", position: "left" }
+    ],
+    []
+  );
   useEffect(() => {
     let didMount = true;
     const start = async () => {
       try {
         setLoading(true);
-        const response = await fetch(`http://hn.algolia.com/api/v1/search?page=${page}`);
+        const response = await fetch(
+          `https://hn.algolia.com/api/v1/search?page=${page}`
+        );
         const json = await response.json();
         if (didMount) {
           setState(json);
@@ -37,7 +45,7 @@ function App() {
       }
     };
     start();
-    return () => didMount = false;
+    return () => (didMount = false);
   }, [page]);
   return (
     <main className="container mx-auto p-4">
@@ -62,26 +70,31 @@ function App() {
                 </tr>
               </thead>
               <tbody className="bg-white">
-                {
-                  state?.hits?.map((hit, index) => (
-                    <tr key={hit?.objectID ?? index}>
-                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        {hit?.num_comments ?? 0}
-                      </td>
-                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        {hit?.points ?? 0}
-                      </td>
-                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        <span className="cursor-pointer" onClick={() => alert('hey, hacker!!! not yet implemented 🤣')}>
-                          ▲
-                        </span>
-                      </td>
-                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
-                        {hit?.title ?? ''} {hit?.url ? (hit.url) : ''} by {hit?.author ?? ''} {moment(hit?.created_at ?? Date.now).fromNow()}
-                      </td>
-                    </tr>
-                  ))
-                }
+                {state?.hits?.map((hit, index) => (
+                  <tr key={hit?.objectID ?? index}>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                      {hit?.num_comments ?? 0}
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                      {hit?.points ?? 0}
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                      <span
+                        className="cursor-pointer"
+                        onClick={() =>
+                          alert("hey, hacker!!! not yet implemented 🤣")
+                        }
+                      >
+                        ▲
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                      {hit?.title ?? ""} {hit?.url ? hit.url : ""} by{" "}
+                      {hit?.author ?? ""}{" "}
+                      {moment(hit?.created_at ?? Date.now).fromNow()}
+                    </td>
+                  </tr>
+                ))}
               </tbody>
             </table>
           </div>
@@ -89,28 +102,38 @@ function App() {
         <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
           <div className="flex-1 flex justify-between">
             <button
-              onClick={() => setPage(page => {
-                if (page > 0) {
-                  setPage(page - 1);
-                }
-              })}
+              onClick={() =>
+                setPage(page => {
+                  if (page > 0) {
+                    setPage(page - 1);
+                  }
+                })
+              }
               type="button"
-              disabled={loading || (page === 0)}
-              className={`${loading || (page === 0) ? 'opacity-50 cursor-not-allowed' : ''} relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150`}
+              disabled={loading || page === 0}
+              className={`${
+                loading || page === 0 ? "opacity-50 cursor-not-allowed" : ""
+              } relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150`}
             >
-              {loading ? 'Loading' : 'Previous'}
+              {loading ? "Loading" : "Previous"}
             </button>
             <button
-              onClick={() => setPage(page => {
-                if ((state?.hits?.length ?? 0) > 0) {
-                  setPage(page + 1);
-                }
-              })}
+              onClick={() =>
+                setPage(page => {
+                  if ((state?.hits?.length ?? 0) > 0) {
+                    setPage(page + 1);
+                  }
+                })
+              }
               type="button"
-              disabled={loading || ((state?.hits?.length ?? 0) === 0)}
-              className={`${loading || ((state?.hits?.length ?? 0) === 0) ? 'opacity-50 cursor-not-allowed' : ''} ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150`}
+              disabled={loading || (state?.hits?.length ?? 0) === 0}
+              className={`${
+                loading || (state?.hits?.length ?? 0) === 0
+                  ? "opacity-50 cursor-not-allowed"
+                  : ""
+              } ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150`}
             >
-              {loading ? 'Loading' : 'Next'}
+              {loading ? "Loading" : "Next"}
             </button>
           </div>
         </div>
