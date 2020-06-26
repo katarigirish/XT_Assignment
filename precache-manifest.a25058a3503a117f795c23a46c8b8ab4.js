a75e06ed6e914f71cd3ad0c2e18519401ff387c7
Girish Katari <katarigirish@gmail.com>
2020-06-26 17:49:13 +0530

    Updates

---
+++ precache-manifest.a25058a3503a117f795c23a46c8b8ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27eea7cebb26a81ce8e54657e7000d86",
+    "url": "/XT_Assignment/index.html"
+  },
+  {
+    "revision": "f158733d2b1bb0fdb542",
+    "url": "/XT_Assignment/static/css/2.63f6feb4.chunk.css"
+  },
+  {
+    "revision": "f158733d2b1bb0fdb542",
+    "url": "/XT_Assignment/static/js/2.9242bca2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/XT_Assignment/static/js/2.9242bca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92cfd125581c56b87dfc",
+    "url": "/XT_Assignment/static/js/main.42452b34.chunk.js"
+  },
+  {
+    "revision": "25af48385ba488dc85c6",
+    "url": "/XT_Assignment/static/js/runtime-main.9b2e13d1.js"
+  }
+]);
